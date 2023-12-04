37821ed9b8f8e7280a7671adbb42df8bfc48ef90
Pradnya Dabhade <70446051+pradnya-wisdom@users.noreply.github.com>
2023-12-04 14:30:57 +0530

    Add files via upload

---
+++ Project_SQL_Sheet.sql
@@ -0,0 +1,52 @@
+-- SQL PROJECT
+
+-- 1. Show how many medal counts present for entire data
+
+
+-- 2. Show count of unique sports present in Olympics
+
+
+-- 3. Show how many different medals won by team India
+
+
+
+-- 4. Show event wise medals won by India show from highest to lowest medals won in order
+
+
+
+-- 5. Show event wise medals won by India in order of year
+
+
+
+-- 6. show country who won maximum medals.
+
+
+-- 7.show top 10 countries whowon gold
+
+
+
+--  8. show in which year did United states won most gold medal
+
+
+
+-- 9. In which sports United States has most medals.
+
+
+
+-- 10. Find top three players who won most medals along with their sports and country
+
+
+
+-- 11. Find player with most gold medals in cycling along with his country.
+
+
+
+-- 12.Find player with most medals (Gold+Silver+Bronze) in Basketball also show his country.
+
+
+-- 13. Find out the count of different medals of the top basketball player Teresa Edwards
+
+
+-- 14. Find out medals won by male,female each year , Export this data and plot graph in excel
+
+
